f4dba360214e448a126fb2386a2ba057a9852b62
Navyasri Maredi <navyasrimaredi.iiitn@gmail.com>
2016-12-06 23:07:49 -0800

    Git initiated

---
+++ app/src/main/java/com/example/navyaspc/helpneedy/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.navyaspc.helpneedy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
