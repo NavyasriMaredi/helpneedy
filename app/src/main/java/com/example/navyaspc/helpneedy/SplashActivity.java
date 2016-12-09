89227332f41ea0362c1dde4e7b2d7eb8c7d7f595
Navyasri Maredi <navyasrimaredi.iiitn@gmail.com>
2016-12-08 21:23:44 -0800

    Included Splash screen and Login screen

--- app/src/main/java/com/example/navyaspc/helpneedy/SplashActivity.java
+++ app/src/main/java/com/example/navyaspc/helpneedy/SplashActivity.java
@@ -1,14 +1,25 @@
 package com.example.navyaspc.helpneedy;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
 public class SplashActivity extends AppCompatActivity {
+    private final static int Splash_Duration = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent loginintent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(loginintent);
+                finish();
+            }
+        }, Splash_Duration);
     }
 }
